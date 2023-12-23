26379010b7e7959f4dbb8eacfbeee237dedfd8fe
Veronika <EugeneJester@mail.ru>
2023-12-24 11:20:42 +1200

    new task_1

---
+++ task_1/description.php
@@ -0,0 +1,8 @@
+<?php
+    $_profession = 'Писатель';
+    $_city_country = 'Копенгаген, Франция';
+    $_email = 'Writing letters with an owl, by place of residence';
+    $_mobile = 'Address: Charlottenlund Palace';
+
+    echo $_profession
+?>
